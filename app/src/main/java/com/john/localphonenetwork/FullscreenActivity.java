a007f51b9255338e685187ffcf79b6ab3b6f8671
jcconnol <jcconnol4@gmail.com>
2019-11-28 07:25:56 -0500

    created full screen app and added socket testing

---
+++ app/src/main/java/com/john/localphonenetwork/FullscreenActivity.java
@@ -0,0 +1,276 @@
+package com.john.localphonenetwork;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+
+    private TextView tvClientMsg,tvServerIP,tvServerPort;
+    private final int SERVER_PORT = 8080; //Define the server port
+
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    private final Handler mHideHandler = new Handler();
+    private View mContentView;
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+    private View mControlsView;
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+            mControlsView.setVisibility(View.VISIBLE);
+        }
+    };
+    private boolean mVisible;
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen);
+
+        mVisible = true;
+        mControlsView = findViewById(R.id.fullscreen_content_controls);
+        mContentView = findViewById(R.id.fullscreen_content);
+
+
+        // Set up the user interaction to manually show or hide the system UI.
+        mContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle();
+            }
+        });
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        findViewById(R.id.send_button).setOnTouchListener(mDelayHideTouchListener);
+
+        tvClientMsg = (TextView) findViewById(R.id.textViewClientMessage);
+        tvServerPort = (TextView) findViewById(R.id.textViewServerPort);
+        tvServerPort.setText(Integer.toString(SERVER_PORT));
+
+        getDeviceIpAddress();
+        //New thread to listen to incoming connections
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    //Create a server socket object and bind it to a port
+                    ServerSocket socServer = new ServerSocket(SERVER_PORT);
+                    //Create server side client socket reference
+                    Socket socClient = null;
+                    //Infinite loop will listen for client requests to connect
+                    while (true) {
+                        //Accept the client connection and hand over communication to server side client socket
+                        socClient = socServer.accept();
+                        //For each client new instance of AsyncTask will be created
+                        ServerAsyncTask serverAsyncTask = new ServerAsyncTask();
+                        //Start the AsyncTask execution
+                        //Accepted client socket object will pass as the parameter
+                        serverAsyncTask.execute(new Socket[] {socClient});
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
+    }
+
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        mControlsView.setVisibility(View.GONE);
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void show() {
+        // Show the system bar
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    /**
+     * Schedules a call to hide() in delay milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    public void sendButtonOnClick(View view) {
+    }
+
+    public void getDeviceIpAddress() {
+        try {
+            //Loop through all the network interface devices
+            Enumeration<NetworkInterface> enumeration;
+            for (enumeration = NetworkInterface
+                    .getNetworkInterfaces(); enumeration.hasMoreElements();) {
+                NetworkInterface networkInterface = enumeration.nextElement();
+                //Loop through all the ip addresses of the network interface devices
+                for (Enumeration<InetAddress> enumerationIpAddr = networkInterface.getInetAddresses(); enumerationIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumerationIpAddr.nextElement();
+                    //Filter out loopback address and other irrelevant ip addresses
+                    if (!inetAddress.isLoopbackAddress() && inetAddress.getAddress().length == 4) {
+                        //Print the device ip address in to the text view
+                        tvServerIP.setText(inetAddress.getHostAddress());
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            Log.e("ERROR:", e.toString());
+        }
+    }
+
+    class ServerAsyncTask extends AsyncTask<Socket, Void, String> {
+        //Background task which serve for the client
+        @Override
+        protected String doInBackground(Socket... params) {
+            String result = null;
+            //Get the accepted socket object
+            Socket mySocket = params[0];
+            try {
+                //Get the data input stream comming from the client
+                InputStream is = mySocket.getInputStream();
+                //Get the output stream to the client
+                PrintWriter out = new PrintWriter(
+                        mySocket.getOutputStream(), true);
+                //Write data to the data output stream
+                out.println("Hello from server");
+                //Buffer the data input stream
+                BufferedReader br;
+                br = new BufferedReader(
+                        new InputStreamReader(is));
+                //Read the contents of the data buffer
+                result = br.readLine();
+                //Close the client connection
+                mySocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //After finishing the execution of background task data will be write the text view
+            tvClientMsg.setText(s);
+        }
+    }
+}
